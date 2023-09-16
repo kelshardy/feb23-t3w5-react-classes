1e816e3d596a204055fe689d3d6f42808498ebfe
kelshardy <14024@coderacademy.edu.au>
2023-09-16 10:53:25 +1000

    Class component init

---
+++ pokemonclass/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import PokemonDisplay from './PokemonDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      {/* PokemonDisplay is an instance of the component */}
+      <PokemonDisplay />
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemonclass/src/PokemonDisplay.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+// Capital R in the 'from' bit is an error
+// import React from "React";
+
+// Extends form Component to make sure that this works as a React Component
+export default class PokemonDisplay extends React.Component{
+    constructor(props){
+        super(props);
+
+        // State stores data per component
+        this.state = {
+            pokemonName: null
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Some content here</h1>
+            </div>
+        )
+    }
+}
