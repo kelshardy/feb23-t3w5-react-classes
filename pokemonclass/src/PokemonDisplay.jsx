d4502c6e420eb9da26c77da67c238e44c1003acf
kelshardy <14024@coderacademy.edu.au>
2023-09-16 11:38:31 +1000

    Pokemon API data fetched

--- pokemonclass/src/PokemonDisplay.jsx
+++ pokemonclass/src/PokemonDisplay.jsx
@@ -13,8 +13,32 @@ export default class PokemonDisplay extends React.Component{
         }
     }
 
-    componentDidMount(){
+    async componentDidMount(){
         console.log("This message will appear once");
+        
+        // Grab a Pokemon and store its name
+        
+        // Generate a random number or use a user-provided number
+        function getRandomPokemonId(){
+			return Math.floor(Math.random() * 1010) + 1
+		}
+
+        // Retrieve the API data
+        let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + getRandomPokemonId()).catch(error => {
+            throw new Error("API failure.");
+        });
+        console.log("Fetch has finished.")
+
+        if (response.status === 404){
+            throw new Error("API did not have data for the requested ID.");
+        }
+
+        // Convert the response into usable JSON 
+        let data = await response.json().catch(error => {
+            throw new Error("API did not return valid JSON.");
+        });
+
+        this.setState({pokemonName: data.name});
     }
 
     componentWillUnmount(){
@@ -24,6 +48,14 @@ export default class PokemonDisplay extends React.Component{
     // Render is how we return JSX up to the React app
     // so that something is displayed in the browser
     render(){
+
+        if (this.state.pokemonName){
+            return (
+                <div>
+                    <h1>{this.state.pokemonName}</h1>
+                </div>
+            )
+        }
         return (
             <div>
                 <h1>Some content here</h1>
